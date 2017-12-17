f3c09c02b72bbff9f0a6d5adc1aad877ae4f32b3
vrjulianti <vrjulianti@github.com>
2017-12-17 09:37:40 +0700

    Third Commit

---
+++ app/src/main/java/com/spnj/tugasfoodsy/Layout_22.java
@@ -0,0 +1,13 @@
+package com.spnj.tugasfoodsy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout_22 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_22);
+    }
+}
